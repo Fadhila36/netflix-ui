819ce055e8d281237f5fecd4fe19317e47c614b2
Muhammad Fadhila Abiyyu Faris <if18.muhammadabiyyu@mhs.ubpkarawang.ac.id>
2022-04-05 01:16:59 +0700

    List

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import "./navbar.scss";
+
+function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png?20190206123158"
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Mavies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img src="https://i.ibb.co/2MTB5qR/fadhila.jpg" alt="" />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
